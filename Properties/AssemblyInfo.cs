1241e10408f5a3f8c6a9662722f1d1194ed5942e
Sabir Süleymanlı <suleymanlisabir3@gmail.com>
2024-05-30 23:36:41 +0300

    add Properties

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+
+[assembly: AssemblyTitle("SimplePaintApp")]
+[assembly: AssemblyDescription("A minimalistic app to paint")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("david95thinkcode ")]
+[assembly: AssemblyProduct("Esgis_Paint")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+
+[assembly: ComVisible(false)]
+[assembly: Guid("9c59d83c-0e8f-49ba-ad2e-91a396173d7f")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
