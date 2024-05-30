7dae6e67cd7b6cf1801cf50239f1c7da3262a003
Sabir Süleymanlı <suleymanlisabir3@gmail.com>
2024-05-30 23:34:42 +0300

    add Home

---
+++ UI/Home.Designer.cs
@@ -0,0 +1,33 @@
+﻿namespace Esgis_Paint
+{
+    partial class Home
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+
+
+        private System.Windows.Forms.Button btn_new;
+        private System.Windows.Forms.Button btn_open;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fichierToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem nouveauDessinToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ouvrirUneImageToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem journalToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem fermerToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.Button btn_close;
+        private System.Windows.Forms.Label label4;
+    }
+}
